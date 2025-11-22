5e197efe59ce748b83f395fb3d68eb3b2521ced8
Siddarth Naik <siddarthnaik2131@gmail.com>
2025-11-22 23:08:16 +0530

    made changes to support macbook

--- camera.py
+++ camera.py
@@ -1,25 +1,12 @@
-# camera.py
-from picamera2 import Picamera2
+import cv2
 
 class CameraStream:
-    def __init__(self, width=640, height=480):
-        print("Starting camera feed...")
-        self.picam2 = Picamera2()
-        self.config = self.picam2.create_preview_configuration(
-            main={"size": (width, height), "format": "RGB888"}
-        )
-        self.picam2.configure(self.config)
-        
-    def start(self):
-        """Starts the camera stream."""
-        self.picam2.start()
-
+    def start(self) -> None:
+        self._cap = cv2.VideoCapture(0)
+    
     def capture_frame(self):
-        """Captures a single frame from the camera."""
-        # capture_array returns a NumPy array
-        return self.picam2.capture_array()
-
+        _ , frame = self._cap.read()
+        return frame
+    
     def stop(self):
-        """Stops the camera stream."""
-        self.picam2.stop()
-        print("Camera feed stopped.")
+        self._cap.release()
