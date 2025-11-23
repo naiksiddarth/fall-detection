4896037a40684190745425293de96dd41e1cbb58
Siddarth Naik <siddarthnaik2131@gmail.com>
2025-11-23 16:20:02 +0530

    add code to recognize frame size of the camera

--- camera.py
+++ camera.py
@@ -1,8 +1,11 @@
 import cv2
+from config import frame_size
 
 class CameraStream:
     def start(self) -> None:
         self._cap = cv2.VideoCapture(0)
+        frame_size.append(int(self._cap.get(cv2.CAP_PROP_FRAME_WIDTH)))
+        frame_size.append(int(self._cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
     
     def capture_frame(self):
         _ , frame = self._cap.read()
