d0be2df9d6499a36b29ab0cedb94a5d7a638fc05
siddarth naik <siddarthnaik2131@gmail.com>
2025-11-23 17:02:31 +0530

    recovered the picamera code

--- camera.py
+++ camera.py
@@ -1,15 +1,28 @@
-import cv2
+# camera.py
+from picamera2 import Picamera2
 from config import frame_size
 
 class CameraStream:
-    def start(self) -> None:
-        self._cap = cv2.VideoCapture(0)
-        frame_size.append(int(self._cap.get(cv2.CAP_PROP_FRAME_WIDTH)))
-        frame_size.append(int(self._cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-    
+    def __init__(self, width=640, height=480):
+        print("Starting camera feed...")
+        self.picam2 = Picamera2()
+        self.config = self.picam2.create_preview_configuration(
+            main={"size": (width, height), "format": "RGB888"}
+        )
+        self.picam2.configure(self.config)
+        
+    def start(self):
+        """Starts the camera stream."""
+        frame_size.append(640)
+        frame_size.append(480)
+        self.picam2.start()
+
     def capture_frame(self):
-        _ , frame = self._cap.read()
-        return cv2.flip(frame, 1)
-    
+        """Captures a single frame from the camera."""
+        # capture_array returns a NumPy array
+        return self.picam2.capture_array()
+
     def stop(self):
-        self._cap.release()
+        """Stops the camera stream."""
+        self.picam2.stop()
+        print("Camera feed stopped.")
