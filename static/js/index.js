2438397fb09a5c289af7efaafa252b330a01d558
Siddarth Naik <siddarthnaik2131@gmail.com>
2025-12-21 17:45:10 +0530

    add javascript

---
+++ static/js/index.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const shutdown = document.getElementById("shutdown")
+    if (shutdown) { // Added a check to ensure element exists
+        shutdown.addEventListener('click', function () {
+            fetch('/shutdown', { method: "POST" })
+        })
+    } else {
+        console.error("Element with ID 'shutdown' not found.");
+    }
+
+
+    const toggleRecord = document.getElementById("record")
+    if (toggleRecord) { // Added a check to ensure element exists
+        toggleRecord.addEventListener('click', function () {
+            fetch("/toggle_record", {
+                method: "POST"
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.is_recording) {
+                        toggleRecord.textContent = "Stop Recording"
+                    } else {
+                        toggleRecord.textContent = "Start Recording"
+                    }
+                })
+        })
+    } else {
+        console.error("Element with ID 'record' not found.");
+    }
+});
